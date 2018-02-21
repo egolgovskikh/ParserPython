5a91ecccaabb282d59309d4cd972e434ffbb312b
Евгений Голговских <egolgovskikh@gmail.com>
2018-02-21 17:52:57 +0300

    v0.2

--- Project/example.py
+++ Project/example.py
@@ -1,1 +1,13 @@
-print(12)
+from urllib3 import request
+from bs4 import BeautifulSoup
+import urllib3
+import urlopen
+
+def get_html(url):
+    response = urllib3.request.urlopen(url)
+    return response.read()
+
+def main():
+    print(get_html("https://www.myscore.ru/football/spain/laliga/fixtures/"))
+    print(12)
+main()
